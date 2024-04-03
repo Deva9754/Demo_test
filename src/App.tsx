72abac7e346b1a7a274b1cd1e8f962b02f5a72e5
Deva9754 <deva.sahu2001@gmail.com>
2024-04-03 22:24:07 +0530

    "added-counter- an d-checkbox feature"

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import AddToDo from "./typecriptWeb/Addtodo"
-import Navbar from "./typecriptWeb/Navbar"
-import Todos from "./typecriptWeb/Todos"
-import "./App.css"
+// import AddToDo from "./typecriptWeb/Addtodo"
+// import Navbar from "./typecriptWeb/Navbar"
+// import Todos from "./typecriptWeb/Todos"
+// import "./App.css"
 
-const App = () => {
-  return (
-   <main>
-      <h1>TODO REACT + TYPESCRIPT </h1>
-      <Navbar />
-      <AddToDo />
-      <Todos />
-   </main>
-  )
-}
+// const App = () => {
+//   return (
+//    <main>
+//       <h1>TODO REACT + TYPESCRIPT </h1>
+//       <Navbar />
+//       <AddToDo />
+//       <Todos />
+//    </main>
+//   )
+// }
 
-export default App
+// export default App
