36d797152b10bb5eae6cf4438371f75cc82b2f7d
Deva9754 <deva.sahu2001@gmail.com>
2024-04-03 22:29:54 +0530

    "Error fixed"

--- src/components/intv1/CheckBox.js
+++ src/components/intv1/CheckBox.js
@@ -1,19 +1,105 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-const CheckBox = () => {
-  const response = [
-    { name: "Devashish" },
+// const CheckBox = () => {
+//   const response = [
+//     { name: "Devashish" },
+//     { name: "Jeevan" },
+//     { name: "Shaillo" },
+//     { name: "Jayesh" },
+//   ];
+//   const [users, setUsers] = useState([]);
+
+//   useEffect(() => {
+//     setUsers(response);
+//   }, []);
+
+//   const handleClick = (e) => {
+//     const { name, checked } = e.target;
+//     if (name === "allSelect") {
+//       let tempUser = users.map((user) => {
+//         return { ...user, isChecked: checked };
+//       });
+//       setUsers(tempUser);
+//     } else {
+//       let tempUser = users.map((user) =>
+//         user.name === name ? { ...user, isChecked: checked } : user
+//       );
+//       setUsers(tempUser);
+//     }
+//   };
+//   const handleChange = () => {};
+//   return (
+//     <div>
+//       <h1>CheckBox Ques</h1>
+//       <div>
+//         <input
+//           type="checkbox"
+//           className="checkall"
+//           name="allSelect"
+//           checked={
+//             users.filter((users) => users?.isChecked !== true).length < 1
+//           }
+//           onChange={handleChange}
+//         />
+//         <label className="checkall"> SelectAll </label>
+//       </div>
+//       {response.map((user) => (
+//         <div>
+//           <input
+//             type="checkbox"
+//             className="checkall"
+//             name="user.name"
+//             checked={user?.isChecked || false}
+//             onChange={handleClick}
+//           />
+//           <label className="checkall"> {user.name} </label>
+//         </div>
+//       ))}
+
+//       {/* <div>
+//         <input type="checkbox" className="checkall" />
+//         <label className="checkall"> SelectAll </label>
+//       </div>
+//       <div>
+//         <input type="checkbox" className="checkall" />
+//         <label className="checkall"> Check1 </label>
+//       </div>
+//       <div>
+//         <input type="checkbox" className="checkall" />
+//         <label className="checkall"> check2 </label>
+//       </div>
+//       <div>
+//         <input type="checkbox" className="checkall" />
+//         <label className="checkall"> check3 </label>
+//       </div>
+//       <div>
+//         <input type="checkbox" className="checkall" />
+//         <label className="checkall"> check4 </label>
+//       </div> */}
+//     </div>
+//   );
+// };
+
+// export default CheckBox;
+
+import React, { useEffect, useState } from "react";
+
+function CheckBox() {
+  const userData = [
     { name: "Jeevan" },
-    { name: "Shaillo" },
-    { name: "Jayesh" },
+    { name: "Manish" },
+    { name: "Prince" },
+    { name: "Arti" },
+    { name: "rahul" },
   ];
+
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    setUsers(response);
+    setUsers(userData);
   }, []);
 
-  const handleClick = (e) => {
+  const handleChange = (e) => {
     const { name, checked } = e.target;
     if (name === "allSelect") {
       let tempUser = users.map((user) => {
@@ -27,57 +113,37 @@ const CheckBox = () => {
       setUsers(tempUser);
     }
   };
-  const handleChange = () => {};
+
   return (
-    <div>
-      <h1>CheckBox Ques</h1>
+    <div style={{ width: "500px" }}>
+      <h3>Select Users</h3>
       <div>
         <input
           type="checkbox"
-          className="checkall"
+          className="form-check-input"
           name="allSelect"
-          checked={
-            users.filter((users) => users?.isChecked !== true).length < 1
-          }
+          // checked={
+          //   users.filter((user) => user?.isChecked !== true).length < 1
+          // }
+          checked={!users.some((user) => user?.isChecked !== true)}
           onChange={handleChange}
         />
-        <label className="checkall"> SelectAll </label>
+        <label className="form-check-label ms-2">All Select</label>
       </div>
-      {response.map((user) => (
-        <div>
+      {users.map((user, index) => (
+        <div key={index}>
           <input
             type="checkbox"
-            className="checkall"
-            name="user.name"
+            className="form-check-input"
+            name={user.name}
             checked={user?.isChecked || false}
-            onChange={handleClick}
+            onChange={handleChange}
           />
-          <label className="checkall"> {user.name} </label>
+          <label className="form-check-label ms-2">{user.name}</label>
         </div>
       ))}
-
-      {/* <div>
-        <input type="checkbox" className="checkall" />
-        <label className="checkall"> SelectAll </label>
-      </div>
-      <div>
-        <input type="checkbox" className="checkall" />
-        <label className="checkall"> Check1 </label>
-      </div>
-      <div>
-        <input type="checkbox" className="checkall" />
-        <label className="checkall"> check2 </label>
-      </div>
-      <div>
-        <input type="checkbox" className="checkall" />
-        <label className="checkall"> check3 </label>
-      </div>
-      <div>
-        <input type="checkbox" className="checkall" />
-        <label className="checkall"> check4 </label>
-      </div> */}
     </div>
   );
-};
+}
 
 export default CheckBox;
